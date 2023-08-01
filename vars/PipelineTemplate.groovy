6f72c77d18fdd8b0474bd575a3f329973fdac821
ifegit <106068556+ifegit@users.noreply.github.com>
2023-08-01 07:57:21 +0100

    Create PipelineTemplate.groovy

---
+++ vars/PipelineTemplate.groovy
@@ -0,0 +1,37 @@
+
+def call(String Repo_URL) {
+    pipeline {
+        options {
+          disableConcurrentBuilds()
+        }
+        agent {
+          label "jenkins-maven"
+        }
+        environment {
+          JIRA_CREDS = credentials('jx-pipeline-issue-jira-jira')
+          JX_HELM3 = "true"
+        }
+        stages {
+          stage('set repo name') {
+              steps {
+                  script {
+                    def gitUrl = env.GIT_URL
+                    def repoName = gitUrl.substring(gitUrl.lastIndexOf('/') + 1, gitUrl.lastIndexOf('.git'))
+                    echo "Repository Name: ${repoName}"
+                    env.APP_NAME = "${repoName}"
+                }
+              }
+          }
+          stage('Update Environment') {
+            when {
+              branch 'master'
+            }
+            steps {
+              container('igt-tools') {
+                sh 'echo "Repository Name Again: $APP_NAME"'
+              }
+            }
+          }
+        }
+    }
+}
