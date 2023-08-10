edfa43ae58a5f327d1104e27b457af7fb0394827
ifegit <106068556+ifegit@users.noreply.github.com>
2023-08-10 23:35:22 +0100

    Update PipelineTemplate.groovy

--- vars/PipelineTemplate.groovy
+++ vars/PipelineTemplate.groovy
@@ -1,50 +1,46 @@
-    def testext(){
-        sh 'pwd && ls -al'
-        sh 'echo "external step"'
+def testext(){
+    sh 'pwd && ls -al'
+    sh 'echo "external step"'
+}
+
+pipeline {
+    options {
+      disableConcurrentBuilds()
     }
-
-
-    
-    pipeline {
-        options {
-          disableConcurrentBuilds()
-        }
-        agent {
-          label "jenkins-maven"
-        }
-        environment {
-          JIRA_CREDS = credentials('jx-pipeline-issue-jira-jira')
-          JX_HELM3 = "true"
-        }
-
-        stages {
-          stage('set repo name') {
-              steps {
-                  script {
-                    def gitUrl = env.GIT_URL
-                    def repoName = gitUrl.substring(gitUrl.lastIndexOf('/') + 1, gitUrl.lastIndexOf('.git'))
-                    echo "Repository Name: ${repoName}"
-                    env.APP_NAME = "${repoName}"
-                }
-              }
-          }
-          stage('Update Environment') {
-            when {
-              branch 'master'
-            }
-            steps {
-              container('igt-tools') {
-                sh 'echo "Repository Name Again: $APP_NAME"'
-              }
+    agent {
+      label "jenkins-maven"
+    }
+    environment {
+      JIRA_CREDS = credentials('jx-pipeline-issue-jira-jira')
+      JX_HELM3 = "true"
+    }
+    stages {
+      stage('set repo name') {
+          steps {
+              script {
+                def gitUrl = env.GIT_URL
+                def repoName = gitUrl.substring(gitUrl.lastIndexOf('/') + 1, gitUrl.lastIndexOf('.git'))
+                echo "Repository Name: ${repoName}"
+                env.APP_NAME = "${repoName}"
             }
           }
-
-          stage('test ext') {
-              steps{
-                  script{
-                      testext()
-                  }
-              }
+      }
+      stage('Update Environment') {
+        when {
+          branch 'master'
+        }
+        steps {
+          container('igt-tools') {
+            sh 'echo "Repository Name Again: $APP_NAME"'
           }
         }
+      }
+      stage('test ext') {
+          steps{
+              script{
+                  testext()
+              }
+          }
+      }
     }
+}
